05352c3c26a5f227cdf6405fbbd6d93a53db3dcd
BLN-Lab <baatarbilguun996@gmail.com>
2021-04-21 13:34:05 +0800

    Lab 7-2

---
+++ app/src/main/java/com/example/lab_7_2/Favourite.java
@@ -0,0 +1,31 @@
+package com.example.lab_7_2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Favourite extends Fragment {
+    ArrayAdapter adapter;
+    String[] list={"Make any program","Making the game","warch the anime"};
+    ListView lv;
+
+    public Favourite() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_song, container, false);
+        lv=view.findViewById(R.id.List);
+        adapter=new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,list);
+        lv.setAdapter(adapter);
+        return view;
+    }
+}
